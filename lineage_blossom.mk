9f0f92a5239cd4a0c769cc337d3137e88fdb45dd
alternoegraha <37102951+alternoegraha@users.noreply.github.com>
2023-07-13 10:20:00 +0000

    blossom: use Google's Raven device fingerprint
    *copied from garden tree
    
    Signed-off-by: alternoegraha <37102951+alternoegraha@users.noreply.github.com>

--- lineage_blossom.mk
+++ lineage_blossom.mk
@@ -19,5 +19,13 @@ PRODUCT_NAME := lineage_blossom
 PRODUCT_DEVICE := blossom
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="raven-user 12 SP2A.220405.004 8233519 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := google/raven/raven:12/SP2A.220405.004/8233519:user/release-keys
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
